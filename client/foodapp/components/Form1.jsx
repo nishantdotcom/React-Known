fee5bc37d6a10e76a4a940d3be238e17d94d9322
nishantkumar1308 <nishantsingh1308@gmail.com>
2023-06-05 15:21:12 +0530

    Update Page form

---
+++ client/foodapp/components/Form1.jsx
@@ -0,0 +1,8 @@
+function Form1() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+export default Form1;
---
+++ client/foodapp/pages/formType1.js
@@ -0,0 +1,10 @@
+import Form1 from "@/components/Form1";
+
+function formType1() {
+  return (
+    <>
+      <Form1 />
+    </>
+  );
+}
+export default formType1;
